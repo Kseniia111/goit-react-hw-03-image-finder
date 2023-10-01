3710c05fe677f0baa7729e216d3944bcfe2394ed
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-01 21:34:37 +0300

    Searchbar css

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,21 +1,24 @@
 import { Component } from 'react';
+import css from './Searchbar.module.css';
 
 export class Searchbar extends Component {
   render() {
     const { onSubmit, onChange, onClickClear, query } = this.props;
     return (
-      <header class="searchbar">
-        <form class="form">
-          <button type="submit" class="button">
-            <span class="button-label">Search</span>
+      <header className={css.searchBar}>
+        <form className={css.cearchForm} onSubmit={onSubmit}>
+          <button type="submit" className={css.searchFormButton}>
+            <span className={css.searchFormButtonLabel}>Search</span>
           </button>
 
           <input
-            class="input"
+            className={css.searchFormInput}
             type="text"
             autocomplete="off"
             autofocus
             placeholder="Search images and photos"
+            onChange={onChange}
+            value={query}
           />
         </form>
       </header>
