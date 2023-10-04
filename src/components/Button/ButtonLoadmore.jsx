9e60cbb55654a5f9416a6d87046e384d77009403
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-04 14:37:49 +0300

    loader & button refactoring

--- src/components/Button/ButtonLoadmore.jsx
+++ src/components/Button/ButtonLoadmore.jsx
@@ -1,17 +1,28 @@
-import { Component } from 'react';
 import css from './ButtonLoadMore.module.css';
 
-export class ButtonLoadMore extends Component {
-  render() {
-    const { label, handleLoadMore } = this.props;
-    return (
-      <button
-        type="button"
-        className={css.ButtonLoadMore}
-        onClick={handleLoadMore}
-      >
-        {label}
-      </button>
-    );
-  }
-}
+export const ButtonLoadMore = ({ label, handleLoadmore }) => {
+  return (
+    <button
+      type="button"
+      className={css.ButtonLoadMore}
+      onClick={this.handleLoadMore}
+    >
+      {label}
+    </button>
+  );
+};
+
+// export class ButtonLoadMore extends Component {
+//   render() {
+//     const { label, handleLoadMore } = this.props;
+//     return (
+//       <button
+//         type="button"
+//         className={css.ButtonLoadMore}
+//         onClick={handleLoadMore}
+//       >
+//         {label}
+//       </button>
+//     );
+//   }
+// }
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,30 +1,55 @@
-import { Component } from 'react';
 import { ColorRing } from 'react-loader-spinner';
 
-export class Loader extends Component {
-  render() {
-    return (
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          margin: '0 auto',
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          right: '0',
-          bottom: '0',
-          zIndex: '9999',
-        }}
-        wrapperClass="blocks-wrapper"
-        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-      />
-    );
-  }
-}
+export const Loader = () => {
+  return (
+    <ColorRing
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="blocks-loading"
+      wrapperStyle={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        margin: '0 auto',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        zIndex: '9999',
+      }}
+      wrapperClass="blocks-wrapper"
+      colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+    />
+  );
+};
+
+// export class Loader extends Component {
+//   render() {
+//     return (
+//       <ColorRing
+//         visible={true}
+//         height="80"
+//         width="80"
+//         ariaLabel="blocks-loading"
+//         wrapperStyle={{
+//           display: 'flex',
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//           height: '100vh',
+//           margin: '0 auto',
+//           position: 'fixed',
+//           top: '0',
+//           left: '0',
+//           right: '0',
+//           bottom: '0',
+//           zIndex: '9999',
+//         }}
+//         wrapperClass="blocks-wrapper"
+//         colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+//       />
+//     );
+//   }
+// }
